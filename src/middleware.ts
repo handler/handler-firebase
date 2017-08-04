d1894e6cf4626d227d6a3054bc59b56445decfc3
Pilwon Huh <pilwon@gmail.com>
2017-08-04 05:17:32 -0400

    Auto-commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,13 @@
 import { Handler, NextFunction, Request, Response } from 'express';
-import { FunctionHandler, FunctionRequest, FunctionRouter,
-         HTTPContext, HTTPContextOptions, HTTPHandler, HTTPRouter } from 'handler.js';
-
-function _sendResponse(res: Response, ctx: HTTPContext) {
-  for (const field of Object.keys(ctx.res.headers)) {
-    res.header(field, ctx.res.headers[field] as any);
-  }
-  if (ctx.res.status) {
-    res.status(ctx.res.status);
-  }
-  if (ctx.res.type) {
-    res.type(ctx.res.type);
-  }
-  if (ctx.res.body) {
-    res.send(ctx.res.body);
-  } else if (ctx.res.status) {
-    res.sendStatus(ctx.res.status);
-  } else {
-    throw new Error('missing response');
-  }
-}
+import { FunctionRouter, HTTPRouter } from 'handler.js';
 
 export function middlewareFromFunctionRouter(router: FunctionRouter): Handler {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const hReq = new FunctionRequest({
+      const hRes = await router.run({
         body: req.body,
         path: req.path,
       });
-      let hRes;
-      for (const middleware of router.middlewares) {
-        hRes = await (middleware as FunctionHandler)(hReq);
-        return res.json(hRes);
-      }
-      const match = router.matchRoute(hReq.path);
-      if (!match) {
-        return next();
-      }
-      hRes = await match.handler(hReq);
       res.json(hRes);
     } catch (err) {
       next(err);
@@ -47,35 +17,30 @@ export function middlewareFromFunctionRouter(router: FunctionRouter): Handler {
 
 export function middlewareFromHTTPRouter(router: HTTPRouter): Handler {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const rewritePath = router._rewritePath(req.path);
-    if (rewritePath) {
-      return res.redirect(302, rewritePath);
-    }
     try {
-      const options: HTTPContextOptions = {
+      const ctx = await router.run({
         body: req.body,
         headers: req.headers,
         method: req.method,
         path: req.path,
         query: req.query,
-      };
-      let ctx = new HTTPContext(options);
-      for (const middleware of router.middlewares) {
-        await (middleware as HTTPHandler)(ctx, (err: Error) => {
-          if (err) {
-            throw err;
-          }
-        });
+      });
+      for (const field of Object.keys(ctx.res.headers)) {
+        res.header(field, ctx.res.headers[field] as any);
       }
-      const match = router.matchRoute(req.method, req.path);
-      console.log(req.method, req.path);
-      if (!match) {
-        return res.sendStatus(404);
+      if (ctx.res.status) {
+        res.status(ctx.res.status);
       }
-      options.params = match.params;
-      ctx = new HTTPContext(options);
-      await match.handler(ctx);
-      _sendResponse(res, ctx);
+      if (ctx.res.type) {
+        res.type(ctx.res.type);
+      }
+      if (ctx.res.body) {
+        res.send(ctx.res.body);
+      } else if (ctx.res.status) {
+        res.sendStatus(ctx.res.status);
+      } else {
+        next(new Error('missing response'));
+      }
     } catch (err) {
       next(err);
     }
